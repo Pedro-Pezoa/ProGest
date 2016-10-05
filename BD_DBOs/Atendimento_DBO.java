db6657bd834d558fe4a660fd7a9d04354dbb9094
PEDRO LUIZ BONDACZUK PEZOA <pedroluizpezoa@gmail.com>
2016-10-05 12:18:24 -0300

    Adição dos DBOs Foi adicionado os DBOs de Cliente e Atendimento

---
+++ BD_DBOs/Atendimento_DBO.java
@@ -0,0 +1,182 @@
+package BD_DBOs;
+
+public class Atendimento_DBO implements Cloneable, Comparable<Atendimento_DBO>
+{
+	protected int codAten, codClien;
+	protected String nomeAten, dataAten, tipoeAten, obsClien, obsAten, statusAten;
+	
+	//---------------------------------------------------------------------------------------------------------------------------//
+    //---------------------------------------------------------Construtor--------------------------------------------------------//
+    //---------------------------------------------------------------------------------------------------------------------------//
+	
+	public Atendimento_DBO(int _novoAten, int _novoCod, String _novoNome, String _novaData, String _novoTipo, String _novaObsClien, String _novaObsAten, String _novoStatus) throws Exception
+	{
+		this.setCodAten(_novoAten);
+		this.setCodClien(_novoCod);
+		this.setNomeAten(_novoNome);
+		this.setDataAten(_novaData);
+		this.setTipoeAten(_novoTipo);
+		this.setObsClien(_novaObsClien);
+		this.setObsAten(_novaObsAten);
+		this.setStatusAten(_novoStatus);
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------//
+    //-----------------------------------------------------Getters e Setters-----------------------------------------------------//
+    //---------------------------------------------------------------------------------------------------------------------------//
+	
+	public int getCodAten() 
+	{
+		return codAten;
+	}
+
+	public void setCodAten(int _codAten) throws Exception
+	{
+		if (_codAten <= 0) throw new Exception("Código Atendente Inválido");
+		this.codAten = _codAten;
+	}
+
+	public int getCodClien() 
+	{
+		return codClien;
+	}
+
+	public void setCodClien(int _codClien) throws Exception
+	{
+		if (_codClien <= 0) throw new Exception("Código Cliente Inválido");
+		this.codClien = _codClien;
+	}
+
+	public String getNomeAten() 
+	{
+		return nomeAten;
+	}
+
+	public void setNomeAten(String _nomeAten) throws Exception
+	{
+		if (_nomeAten == null || _nomeAten.equals("")) throw new Exception("Nome do Atendente Inválido");
+		this.nomeAten = _nomeAten;
+	}
+
+	public String getDataAten() 
+	{
+		return dataAten;
+	}
+
+	public void setDataAten(String _dataAten) throws Exception
+	{
+		if (_dataAten == null || _dataAten.equals("")) throw new Exception("Data do Atendimento Inválido");
+		this.dataAten = _dataAten;
+	}
+
+	public String getTipoeAten() 
+	{
+		return tipoeAten;
+	}
+
+	public void setTipoeAten(String _tipoeAten) throws Exception
+	{
+		if (_tipoeAten == null || _tipoeAten.equals("")) throw new Exception("Tipo do Atendimento Inválido");
+		this.tipoeAten = _tipoeAten;
+	}
+
+	public String getObsClien() 
+	{
+		return obsClien;
+	}
+
+	public void setObsClien(String _obsClien) throws Exception
+	{
+		if (_obsClien == null || _obsClien.equals("")) throw new Exception("Observação 1 Inválido");
+		this.obsClien = _obsClien;
+	}
+
+	public String getObsAten() 
+	{
+		return obsAten;
+	}
+
+	public void setObsAten(String _obsAten) throws Exception
+	{
+		if (_obsAten == null || _obsAten.equals("")) throw new Exception("Observação 2 Inválido");
+		this.obsAten = _obsAten;
+	}
+
+	public String getStatusAten() 
+	{
+		return this.statusAten;
+	}
+	
+	public void setStatusAten(String _novoStatus) 
+	{
+		this.statusAten = _novoStatus;
+	}
+	
+	//------------------------------------------------------------------------------------------------------------------------------//
+    //-----------------------------------------------------Métodos Apocalipticos----------------------------------------------------//
+    //------------------------------------------------------------------------------------------------------------------------------//
+	
+	@Override
+	public int hashCode() 
+	{
+		int result = super.hashCode();
+		result *= 7 + this.codAten;
+		result *= 7 + this.codClien;
+		result *= 7 + this.dataAten.hashCode();
+		result *= 7 + this.nomeAten.hashCode();
+		result *= 7 + this.obsAten.hashCode();
+		result *= 7 + this.obsClien.hashCode();
+		result *= 7 + this.statusAten.hashCode();
+		result *= 7 + this.tipoeAten.hashCode();
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object _obj) 
+	{
+		if (this == _obj)return true;
+		if (_obj == null)return false;
+		
+		if (getClass() == _obj.getClass())
+		{
+			Atendimento_DBO outro = (Atendimento_DBO) _obj;
+			if (this.codAten == outro.codAten && this.codClien == outro.codClien && this.nomeAten.equals(outro.nomeAten) && this.dataAten.equals(outro.dataAten) &&
+				this.tipoeAten.equals(outro.tipoeAten) && this.obsAten.equals(outro.obsAten) && this.obsClien.equals(outro.obsClien) && this.statusAten.equals(outro.statusAten))
+				return true;
+		}
+		
+		if (_obj instanceof String)
+		{
+			String outro = (String) _obj;
+			if (this.toString().equals(outro))return true;
+		}
+		return false;
+	}
+
+	public Atendimento_DBO clone()
+	{
+		return new Atendimento_DBO(this);
+	}
+	
+	public Atendimento_DBO(Atendimento_DBO _outro) 
+	{
+		this.codClien = _outro.codClien;
+		this.codAten = _outro.codAten;
+		this.nomeAten = _outro.nomeAten;
+		this.dataAten = _outro.dataAten;
+		this.codClien = _outro.codClien;
+		this.tipoeAten = _outro.tipoeAten;
+		this.tipoeAten = _outro.tipoeAten;
+		this.obsAten = _outro.obsAten;
+		this.obsClien = _outro.obsClien;
+		this.statusAten = _outro.statusAten;
+	}
+
+	@Override
+	public int compareTo(Atendimento_DBO _outro) 
+	{
+		if (this.codAten > _outro.codAten) return 1;
+		if (this.codAten < _outro.codAten) return -1;
+		return 0;
+	}
+}
---
+++ BD_DBOs/Cliente_DBO.java
@@ -0,0 +1,132 @@
+package BD_DBOs;
+
+public class Cliente_DBO implements Cloneable, Comparable<Cliente_DBO>
+{
+	protected int codClien;
+	protected String nomeClien, teleClien, emailClien;
+	
+	//---------------------------------------------------------------------------------------------------------------------------//
+    //---------------------------------------------------------Construtor--------------------------------------------------------//
+    //---------------------------------------------------------------------------------------------------------------------------//
+	
+	public Cliente_DBO(int _novoCod, String _novoNome, String _novoTele,String _novoEmail) throws Exception
+	{
+		this.setCodClien(_novoCod);
+		this.setNomeClien(_novoNome);
+		this.setEmailClien(_novoEmail);
+		this.setTeleClien(_novoTele);
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------//
+    //-----------------------------------------------------Getters e Setters-----------------------------------------------------//
+    //---------------------------------------------------------------------------------------------------------------------------//
+	
+	public int getCodClien() 
+	{
+		return codClien;
+	}
+
+	public void setCodClien(int _codClien) throws Exception
+	{
+		if (_codClien <= 0) throw new Exception("Código Inválido");
+		this.codClien = _codClien;
+	}
+
+	public String getNomeClien() 
+	{
+		return nomeClien;
+	}
+
+	public void setNomeClien(String _nomeClien) throws Exception
+	{
+		if (_nomeClien == null || _nomeClien.equals("")) throw new Exception("Nome do Cliente Inválido");
+		this.nomeClien = _nomeClien;
+	}
+
+	public String getTeleClien() 
+	{
+		return teleClien;
+	}
+
+	public void setTeleClien(String _teleClien) throws Exception
+	{
+		if (_teleClien == null || _teleClien.equals("")) throw new Exception("Telefone do Cliente Inválido");
+		this.teleClien = _teleClien;
+	}
+
+	public String getEmailClien() 
+	{
+		return emailClien;
+	}
+
+	public void setEmailClien(String _emailClien) throws Exception
+	{
+		if (_emailClien == null || _emailClien.equals("")) throw new Exception("Email do Cliente Inválido");
+		this.emailClien = _emailClien;
+	}
+	
+	//------------------------------------------------------------------------------------------------------------------------------//
+    //-----------------------------------------------------Métodos Apocalipticos----------------------------------------------------//
+    //------------------------------------------------------------------------------------------------------------------------------//
+	
+	@Override
+	public String toString() 
+	{
+		return "Código do Client: " + this.codClien + " Nome do Cliente: " + this.nomeClien + 
+			   " Telefone do Cliente: " + this.teleClien + " Email do Cliente=" + this.emailClien;
+	}
+	
+	@Override
+	public int hashCode() 
+	{
+		int result = super.hashCode();
+		result *= 7 + new Integer(this.codClien).hashCode();
+		result *= 7 + this.emailClien.hashCode();
+		result *= 7 + this.nomeClien.hashCode();
+		result *= 7 + this.teleClien.hashCode();
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object _obj) 
+	{
+		if (this == _obj)return true;
+		if (_obj == null)return false;
+		
+		if (getClass() == _obj.getClass())
+		{
+			Cliente_DBO outro = (Cliente_DBO) _obj;
+			if (this.codClien == outro.codClien && this.nomeClien.equals(outro.nomeClien) &&
+				this.emailClien.equals(outro.emailClien) &&this.teleClien.equals(outro.teleClien))
+				return true;
+		}
+		
+		if (_obj instanceof String)
+		{
+			String outro = (String) _obj;
+			if (this.toString().equals(outro)) return true;
+		}
+		return false;
+	}
+	
+	public Cliente_DBO clone()
+	{
+		return new Cliente_DBO(this);
+	}
+	
+	public Cliente_DBO(Cliente_DBO _outro) 
+	{
+		this.codClien = _outro.codClien;
+		this.nomeClien = _outro.nomeClien;
+		this.emailClien = _outro.emailClien;
+		this.teleClien = _outro.teleClien;
+	}
+
+	@Override
+	public int compareTo(Cliente_DBO _outro) 
+	{
+		if (this.codClien > _outro.codClien) return 1;
+		if (this.codClien < _outro.codClien) return -1;
+		return 0;
+	}
+}
